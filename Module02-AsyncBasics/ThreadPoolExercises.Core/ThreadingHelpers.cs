531a5a3679892f6d259208f4e79c15729c944927
Jindrich Cincibuch <jindrich.cincibuch@gmail.com>
2020-07-31 18:06:51 +0200

    Homework 2 done.

--- Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
+++ Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
@@ -5,26 +5,64 @@ namespace ThreadPoolExercises.Core
 {
     public class ThreadingHelpers
     {
-        public static void ExecuteOnThread(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
+        public static void ExecuteOnThread(Action action, int repeats, CancellationToken token = default,
+            Action<Exception>? errorAction = null)
         {
             // * Create a thread and execute there `action` given number of `repeats` - waiting for the execution!
             //   HINT: you may use `Join` to wait until created Thread finishes
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            var thread = new Thread(() =>
+            {
+                for (int i = 0; i < repeats; i++)
+                {
+                    try
+                    {
+                        token.ThrowIfCancellationRequested();
+                        action();
+                    }
+                    catch (Exception e)
+                    {
+                        errorAction?.Invoke(e);
+                        break;
+                    }
+                }
+            });
 
-
+            thread.Start();
+            thread.Join();
         }
 
-        public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
+        public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default,
+            Action<Exception>? errorAction = null)
         {
             // * Queue work item to a thread pool that executes `action` given number of `repeats` - waiting for the execution!
             //   HINT: you may use `AutoResetEvent` to wait until the queued work item finishes
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            using var autoResetEvent = new AutoResetEvent(false);
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                for (int i = 0; i < repeats; i++)
+                {
+                    try
+                    {
+                        token.ThrowIfCancellationRequested();
+                        action();
+                    }
+                    catch (Exception e)
+                    {
+                        errorAction?.Invoke(e);
+                        break;
+                    }
+                }
 
+                autoResetEvent.Set();
+            });
 
+            autoResetEvent.WaitOne();
         }
     }
 }
